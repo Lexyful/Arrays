d5ae72651eb5feceff0ab75ed34562f5d657f013
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-09 15:40:39 -0700

    Add another array

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var Array = ["Liam", "John", "Sarah", "Tom", "Madison"]
 console.log(Array[2])
 console.log(Array[4])
 console.log(Array[0])
+
+var Places = ["Coppenhagen", "Rome", "Brussels", "Iceland", "Montreal"]
+console.log(Places.shift())
+console.log(Places.shift(), Places.shift())
